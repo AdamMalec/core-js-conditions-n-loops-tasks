e0adf57eadda8c5e9cfc2f88763a5234e2d68921
Adam Malec <add.malec@gmail.com>
2024-01-28 21:21:17 +0300

    feat: complete sortByAsc (no-speed-test)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -370,8 +370,22 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const result = arr;
+
+  for (let i = 1; i < arr.length; i += 1) {
+    const cur = arr[i];
+
+    let j = i;
+    while (j > 0 && cur < arr[j - 1]) {
+      result[j] = arr[j - 1];
+      j -= 1;
+    }
+
+    result[j] = cur;
+  }
+
+  return result;
 }
 
 /**
